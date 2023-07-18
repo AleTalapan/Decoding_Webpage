dcc58aaed6de2f494e3ae1fdca5465107fc9bbc9
Alexandra <aletalapan@gmail.com>
2023-07-18 13:40:38 +0300

    Write in a file

--- decode2.py
+++ decode2.py
@@ -7,5 +7,7 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 all_p_cn_text_body = soup.select("div.body__inner-container > p")
 
-for elem in all_p_cn_text_body:
-    print(elem.getText())
+with open('lines.txt', 'w') as file:
+    for elem in all_p_cn_text_body:
+        file.write(elem.text + '\n')
+
